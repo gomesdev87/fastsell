91c8a1e5f3ae1778c4a377a87b5e461513c00a45
gomesdev87 <134112503+gomesdev87@users.noreply.github.com>
2025-09-30 14:24:15 +0000

    inicializando o projeto spring boot

---
+++ src/test/java/com/fastsell/fastsell/FastsellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastsell.fastsell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastsellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
